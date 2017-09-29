8f6b0ca80a46bf0e0df170cae55d808c12157d37
janbrogger <jan.brogger@holbergeeg.com>
2017-09-29 15:52:25 +0200

    Clear_results: Added warnings.

--- ctap/src/core/CTAP_clear_results.m
+++ ctap/src/core/CTAP_clear_results.m
@@ -31,11 +31,20 @@ function deletefilesandfolders(root)
     rootDir = dir(root);
     for i = 1:length(rootDir)
         if strcmp(rootDir(i).name,'.')==0 && strcmp(rootDir(i).name,'..')==0
-            if rootDir(i).isdir
-                deletefilesandfolders(fullfile(rootDir(i).folder, rootDir(i).name));
-                rmdir(fullfile(rootDir(i).folder, rootDir(i).name));
+            if rootDir(i).isdir                                    
+                dir2delete = fullfile(rootDir(i).folder, rootDir(i).name);
+                deletefilesandfolders(dir2delete);
+                try                     
+                    rmdir(dir2delete);
+                catch
+                    warning(['Couldn''t rmdir ' dir2delete]);
+                end
             else
-                delete(fullfile(rootDir(i).folder, rootDir(i).name));                                               
+                try 
+                    file2delete = fullfile(rootDir(i).folder, rootDir(i).name);                    delete(file2delete);
+                catch
+                    warning(['Couldn''t delete' file2delete]);
+                end
             end
         end
     end
