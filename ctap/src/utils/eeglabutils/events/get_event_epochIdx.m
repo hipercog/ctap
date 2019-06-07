8299cb6bdbb135cc1267722276f087acf5a5e3bb
zenben <ben.cowley@helsinki.fi>
2019-06-07 23:03:06 +0300

    Added EEG-epoched-check condition to get_event_epochIdx.m

--- ctap/src/utils/eeglabutils/events/get_event_epochIdx.m
+++ ctap/src/utils/eeglabutils/events/get_event_epochIdx.m
@@ -2,6 +2,9 @@ function idx = get_event_epochIdx(EEG, event)
 %GET_EVENT_EPOCHIDX: find epochs with wanted event
 % 3 lines to GET AN INDEX - must be easier way?
 
+    if isempty(EEG.epoch)
+        error('get_event_epochIdx:bad_EEG', 'EEG must be epoched!')
+    end
     idx = squeeze(struct2cell(EEG.epoch));
     idx = squeeze(idx(ismember(fieldnames(EEG.epoch), 'eventtype'), :));
     idx = cell2mat(cellfun(@(x) any(strcmpi(x, event)), idx, 'Un', 0));
